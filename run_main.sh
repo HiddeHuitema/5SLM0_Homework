8e169c55da327d3aaef71828497fa27d488a5423
Hidde Huitema <h.r.huitema@student.tue.nl>
2024-03-15 09:57:36 +0100

    increase wall time estimate

--- run_main.sh
+++ run_main.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --partition=gpu
-#SBATCH --time=0:10:00
+#SBATCH --time=1:00:00
 
 cd /gpfs/home2/scur0764/FinalAssignment
 mkdir wandb/$SLURM_JOBID
