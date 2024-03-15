19e06526c146025b2a4f1270336f353a3ddb8b32
Hidde Huitema <h.r.huitema@student.tue.nl>
2024-03-15 09:59:47 +0100

    reduce batch size and downsizing factor

--- run_container.sh
+++ run_container.sh
@@ -7,4 +7,4 @@ wandb login
 
 torchrun --nnodes=1 --nproc_per_node=1 train.py \
          --data_path "/gpfs/work5/0/jhstue005/JHS_data/CityScapes" \
-         --resizing_factor 4
+         --resizing_factor 2
