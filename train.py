5ac98a453ee26c2cb184e093c3c13e967982bf3a
Hidde Huitema <h.r.huitema@student.tue.nl>
2024-04-03 09:44:06 +0200

    reduce batch size

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ def get_arg_parser():
     parser = ArgumentParser()
     parser.add_argument("--data_path", type=str, default="./Datasets/CityScapes", help="Path to the data")
     parser.add_argument("--epochs",type = int, default = 50, help = "Amount of epochs for training")
-    parser.add_argument("--batch_size",type = int, default = 30, help = "Batch size for training")
+    parser.add_argument("--batch_size",type = int, default = 20, help = "Batch size for training")
     parser.add_argument("--resizing_factor" ,type = int, default = 16, help = "Resizing factor for the size of the images, makes training on cpu faster for testing purposes")
     parser.add_argument("--n_workers", type = int, default = 1, help = "Number of workers for dataloading" )
     """add more arguments here and change the default values to your needs in the run_container.sh file"""
